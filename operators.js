de7e1ab545434899557b068e94815c87a5e73d83
Pratham Jagga <pratham.jagga@cloudeq.com>
2023-07-14 11:48:00 +0530

    add operator.js

---
+++ operators.js
@@ -0,0 +1,45 @@
+// javascript program to demonstrate different types of operators
+
+// (1) Assignment operator
+var a = 2
+console.log(a)
+
+// (2) Arithmetic operators
+console.log(20+10) // addition
+console.log(20%10) // modulo
+console.log(20-10) // subtraction
+console.log(20*10) // multiplication
+console.log(20/10) // division
+
+// (3) Ternary operator
+1==2 ? console.log("yes") : console.log("no")
+var a = 1==2? 1 : 2
+
+// (4) Relational / Comparison operators
+console.log(2>1)
+console.log(2=="1")
+console.log(2==="1")
+console.log(2<1)
+console.log(2>=1)
+console.log(2<=1)
+console.log(1!=1)
+
+// (5) Unary operators
+console.log(2++)
+console.log(2--)
+console.log(!true)
+console.log(--2)
+console.log(++2)
+
+// (6) Logical operators
+console.log(1==1 || 2==3)
+console.log(1==1 && 2==3)
+
+// (7) Bitwise operators
+console.log(2&3)
+console.log(2|3)
+console.log(2^3)
+console.log(~2)
+console.log(2<<3)
+console.log(2>>3)
+console.log(2>>>3)
